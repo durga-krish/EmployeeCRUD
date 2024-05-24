7366f452998de8986cc0f8f1efcd7ca0435dd756
Durga <durgakrish3553@gmail.com>
2024-05-24 16:46:21 +0530

    Employee CRUD Feat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./pages/header/Header";
+import {Route, Routes} from "react-router-dom";
+import Dashboard from "./pages/dashboard/Dashboard";
+import PostEmployee from "./pages/employee/PostEmployee";
+import UpdateEmployee from "./pages/employee/UpdateEmployee";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header/>
+        <Routes>
+            <Route path='/' element={<Dashboard/>} />
+            <Route path='/employee' element={<PostEmployee/>} />
+            <Route path='/employee/:id' element={<UpdateEmployee/>} />
+
+
+        </Routes>
+
+
+    </>
   );
 }
 
